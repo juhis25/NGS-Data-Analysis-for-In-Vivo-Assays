4a69538efdaba631d0dbdd521d260fd0abcbf81e
juhis25 <123091089+juhis25@users.noreply.github.com>
2024-05-22 17:03:32 -0400

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,27 @@
+
+### Script File: `download_data.sh`
+
+Create a file named `download_data.sh` with the following content:
+
+```bash
+#!/bin/bash
+
+# Create directories for storing data
+mkdir -p data/fastq
+mkdir -p data/reference
+
+# Change directory to data/fastq
+cd data/fastq
+
+# Download example data from SRA
+fastq-dump --split-files SRR12345678
+
+# Rename files for consistency (if necessary)
+mv SRR12345678_1.fastq sample_R1.fastq
+mv SRR12345678_2.fastq sample_R2.fastq
+
+# Download the reference genome
+cd ../reference
+wget ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz
+gunzip Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz
+mv Homo_sapiens.GRCh37.75.dna.primary_assembly.fa hg19.fasta
